a5557807ffce4412c1f952a4a94bc073fd1c7abd
agustiyann <agustiyaninfo@gmail.com>
2020-11-17 15:11:23 +0700

    Update UI

--- app/src/main/java/com/masscode/simpletodolist/MainActivity.kt
+++ app/src/main/java/com/masscode/simpletodolist/MainActivity.kt
@@ -3,9 +3,14 @@ package com.masscode.simpletodolist
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.masscode.simpletodolist.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,5 +19,12 @@ class MainActivity : AppCompatActivity() {
 
         val toolbar = binding.mainToolbar
         setSupportActionBar(toolbar)
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
